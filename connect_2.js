1836a075e3871ed6a472d7112e658c277c406910
Admir Monteiro <admirmonteiro@icloud.com>
2015-05-13 00:06:26 -0400

    new connect 2 to connect to the micro BLE device : works incoming values: converted values, checks for rssi values to disconnect : will work on rescan after weak signal: will work on keyboard interrupt to disconnect and reconnect

--- connect_2.js
+++ connect_2.js
@@ -1,6 +1,7 @@
 var noble = require('../index');
 var childProcess = require('child_process');
 var serviceUUIDs = ["d393a16025f4"];
+var exitHandlerBound = false;
 //checking the state of system and scan for BLE devices
 noble.on('stateChange', function (state) {
     if (state === 'poweredOn') {
@@ -10,30 +11,29 @@ noble.on('stateChange', function (state) {
         console.log('not scanning.. will rescan for : ' + serviceUUIDs);
         setTimeout(function (error) {
         }, 2000);
-        function runScript(scriptPath, callback) {
-
-            // keep track of whether callback has been invoked to prevent multiple invocations
-            var invoked = false;
-
-            var process = childProcess.fork(scriptPath);
-
-            // listen for errors as they may prevent the exit event from firing
-            process.on('error', function (err) {
-                if (invoked) return;
-                invoked = true;
-                callback(err);
-            });
-
-            // execute the callback once the process has finished running
-            process.on('exit', function (code) {
-                if (invoked) return;
-                invoked = true;
-                var err = code === 0 ? null : new Error('exit code ' + code);
-                callback(err);
-            });
-
-        }
-
+        //function runScript(scriptPath, callback) {
+        //
+        //    // keep track of whether callback has been invoked to prevent multiple invocations
+        //    var invoked = false;
+        //
+        //    var process = childProcess.fork(scriptPath);
+        //
+        //    // listen for errors as they may prevent the exit event from firing
+        //    process.on('error', function (err) {
+        //        if (invoked) return;
+        //        invoked = true;
+        //        callback(err);
+        //    });
+        //
+        //    // execute the callback once the process has finished running
+        //    process.on('exit', function (code) {
+        //        if (invoked) return;
+        //        invoked = true;
+        //        var err = code === 0 ? null : new Error('exit code ' + code);
+        //        callback(err);
+        //    });
+        //
+        //}
 // Now we can run a script and invoke a callback when complete, e.g.
         runScript('./connect_2.js', function (err) {
             if (err) throw err;
@@ -49,7 +49,10 @@ noble.on('discover', function (peripheral) {
         //var serviceUUIDs = ["fe1a56c98efa"];
         if (serviceUUIDs == peripheral.uuid) {
             console.log('Trying to connect ...');
-            console.log('Connected to peripheral: ' + peripheral.uuid);
+            console.log('Connected to peripheral: ' + peripheral.uuid + ' RSSI:' + peripheral.rssi);
+            console.log('Discovering Services available ...');
+
+
             peripheral.discoverServices(null, function (error, services) {
                 console.log('Discovered the following services:');
                 for (var i in services) {
@@ -73,11 +76,93 @@ noble.on('discover', function (peripheral) {
                         console.log('Notification on for :' + manufacturerNameCharacteristic.name);
                     });
                     manufacturerNameCharacteristic.on('read', function (data, onNotification) {
-                        console.log('Place_1 :', data[0].toString(32));
-                        console.log('Place_2 :', data[1].toString(32));
+                        console.log('hrv :', parseInt(data[1], 16));
+                        console.log('bpm :', parseInt(data[0], 16));
+                        peripheral.updateRssi(function (error, rssi) {
+                            console.log('RSSI: (' + peripheral.rssi + ')')
+                            if (peripheral.rssi < -50) {
+                                console.log('weak service...will stop scanning... and rescan');
+                                peripheral.disconnect(function (error) {
+                                    console.log('disconnected from peripheral: ' + peripheral.uuid);
+                                });
+                            }
+                            ;
+                            //console.log(peripheral.uuid + ' RSSI:' + peripheral.rssi+ ' update RSSI + ' + rssi + ' : Error :' + error);
+                        });
+
                     });
+
                 });
             });
+
         }
     });
+
 });
+
+//----------------Functions --------------------------------------------------------
+function runScript(scriptPath, callback) {
+
+    // keep track of whether callback has been invoked to prevent multiple invocations
+    var invoked = false;
+
+    var process = childProcess.fork(scriptPath);
+
+    // listen for errors as they may prevent the exit event from firing
+    process.on('error', function (err) {
+        if (invoked) return;
+        invoked = true;
+        callback(err);
+    });
+
+    // execute the callback once the process has finished running
+    process.on('exit', function (code) {
+        if (invoked) return;
+        invoked = true;
+        var err = code === 0 ? null : new Error('exit code ' + code);
+        callback(err);
+    });
+
+}
+//*****************************Comments**********************************************
+//
+//var disc = function disc(){
+//    peripheral.disconnect(function(error) {
+//        console.log('disconnected from peripheral: ' + peripheral.uuid);
+//    });
+//};
+
+//
+//var exitHandler = function exitHandler(peripheral) {
+//
+//        peripheral.disconnect(function (err) {
+//
+//        });
+//
+//        if (serviceUUIDs == peripheral.uuid) {
+//            console.log('Disconnected from : (' + serviceUUIDs + ')');
+//        }
+//
+//        //peripheral.on('disconnect',function () {
+//        //    if (serviceUUIDs == peripheral.uuid) {
+//        //        console.log('Disconnected from : ('+ serviceUUIDs+')');
+//        //    }
+//        //});
+//    //end process after 2 more seconds
+//    setTimeout(function(){
+//        process.exit();
+//    }, 2000);
+//};
+//
+
+//peripheral.on('disconnect', function() {
+//    console.log('on -> disconnect');
+//});
+//
+//var exitHandler = function exitHandler() {
+//    peripherals.forEach(function(peripheral) {
+//        console.log('Disconnecting from ' + peripheral.uuid + '...');
+//        peripheral.disconnect( function(){
+//            console.log('disconnected');
+//        });
+//    });
